c6687bcf93b5460704c1ed22134f215955acdd6f
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-25 18:35:40 +0300

    Grid and Dot Backgrounds Aceternity UI
    install mini-svg-data-uri & tailwindcss-animate

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
-  return <div className="pb-20 pt-36 ">Hero</div>;
+  return (
+    <div className="pb-20 pt-36 ">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+          Backgrounds
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
