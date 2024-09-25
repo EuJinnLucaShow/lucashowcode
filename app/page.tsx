2a3759c5f6222168e2f31dd063e00784edef7cba
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-25 16:50:45 +0300

    Installation framer-motion clsx tailwind-merge

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
-      <div>
-        <h1>Hello Portfolio</h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-v-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
---
+++ components/Hero.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hero = () => {
+  return <div className="pb-20 pt-36 ">Hero</div>;
+};
+
+export default Hero;
