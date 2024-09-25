9aa406591a70ffe80ea92e0bdf767f13bfedae1c
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-25 20:28:41 +0300

    Update TextGenerateEffect.tsx

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -54,7 +54,7 @@ export const TextGenerateEffect = ({
 
   return (
     <div className={cn("font-bold", className)}>
-      <div className="mt-4">
+      <div className="my-4">
         <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
           {renderWords()}
         </div>
