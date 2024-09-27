71713b151a7dc46c4ed7389fdf1fc9733b435b28
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-27 09:50:32 +0300

    Update Grid.tsx

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,26 +6,14 @@ const Grid = () => {
     <section id="about">
       <BentoGrid className="w-full py-20">
         {gridItems.map(
-          ({
-            id,
-            title,
-            description,
-            className,
-            img,
-            imgClassName,
-            titleClassName,
-            spareImg,
-          }) => (
+          ({ id, title, description, className, titleClassName }) => (
             <BentoGridItem
               id={id}
               key={id}
               title={title}
               description={description}
               className={className}
-              img={img}
-              imgClassName={imgClassName}
               titleClassName={titleClassName}
-              spareImg={spareImg}
             />
           )
         )}
