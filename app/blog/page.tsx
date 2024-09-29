c768ea0cdcb8941c787aaa8b05071c4f09b7f60b
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-29 18:35:09 +0300

    no-explicit-any
    .eslintrc.cjs
    module.exports = {
      "rules": {
        "@typescript-eslint/no-explicit-any": "error"
      }
    };

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -23,15 +23,12 @@ export default async function Blog() {
               fill="blue"
             />
           </div>
-          <div className="py-5">
+          <div className=" py-5">
             {posts?.length > 0 ? (
               posts.map((post: any) => <BlogItem key={post._id} blog={post} />)
             ) : (
-              <p>No posts found</p>
+              <p className="text-slate-200">No posts found</p>
             )}
-          </div>
-          <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
           </div>
         </div>
       </div>
