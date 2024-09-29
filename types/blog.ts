990a47214be18183363b9e5168c5e1a4d62f4bc9
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-29 21:43:11 +0300

    Update blog.ts

--- types/blog.ts
+++ types/blog.ts
@@ -3,7 +3,9 @@ import { PortableTextBlock } from "sanity";
 export type Blog = {
   _id: number;
   title: string;
-  slug: string;
+  slug: {
+    current: string;
+  };
   metadata: string;
   body: PortableTextBlock[];
   mainImage: any;
